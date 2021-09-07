0cfe1be1d6eeb8563188fc08de0c320dd4e14f82
Andrew Singham <z5208642@ad.unsw.edu.au>
2021-09-08 09:35:37 +1000

    Now strips < and >

--- javascript/sanitise_input.js
+++ javascript/sanitise_input.js
@@ -1,5 +1,7 @@
 function sanitiseInput(input) {
     // Sanitise(?) input
+    input = input.split(">").join("");
+    input = input.split("<").join("");
     input = input.split(" ").join("");
     input = input.toUpperCase();
     return input;
