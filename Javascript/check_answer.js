13a9e09802d225c65f071fc7e7dc9c7f6f3d5504
Andrew Singham <z5208642@ad.unsw.edu.au>
2021-09-07 13:04:51 +1000

    Fixing up file structure

--- Javascript/check_answer.js
+++ Javascript/check_answer.js
@@ -1,16 +1,12 @@
 function checkAnswer(input, answer) {
-    // Sanitise(?) input
-    input = input.split(" ").join("");
-    input = input.toUpperCase();
-  
     var encoded = input;
     for (let i = 0; i < 5; i++) {
-      encoded = btoa(encoded);
+        encoded = btoa(encoded);
     }
   
     if (encoded == answer) {
-      return true;
+        return true;
     } else {
-      return false;
+        return false;
     }
 }
---
+++ Javascript/sanitise_input.js
@@ -0,0 +1,6 @@
+function sanitiseInput(input) {
+    // Sanitise(?) input
+    input = input.split(" ").join("");
+    input = input.toUpperCase();
+    return input;
+}
